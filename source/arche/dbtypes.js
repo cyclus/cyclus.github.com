efda272d7010ad4331806cc1b28fed4eabfd2beb
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-01 15:04:54 -0500

    hotfix for json issue

--- source/arche/dbtypes.js
+++ source/arche/dbtypes.js
@@ -1056,7 +1056,7 @@ var dbdata =
     '[142, "VL_MAP_VL_STRING_MAP_INT_DOUBLE", "std::map<std::string, std::map<int, double> >", 3, "HDF5", "v1.3", 0],' +
     '[143, "VL_MAP_VL_STRING_VL_MAP_INT_DOUBLE", "std::map<std::string, std::map<int, double> >", 3, "HDF5", "v1.3", 0],' +
     '[0, "BOOL", "bool", 0, "HDF5", "v1.3", 1]' +
-    ']';
+    ']]';
 
 dbdata = dbdata.replace(new RegExp('<', 'g'), '&lt;')
                .replace(new RegExp('>', 'g'), '&gt;');
