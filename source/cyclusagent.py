39f41eea478a8749aa4d7caf4a749fc8bcac3ad2
Matthew Gidden <matthew.gidden@gmail.com>
2015-05-18 10:55:25 -0500

    go down one more level of aliasing if alias[0] is not a string. fixes #143

--- source/cyclusagent.py
+++ source/cyclusagent.py
@@ -309,7 +309,12 @@ class CyclusAgent(Directive):
                 continue
 
             alias = info.get('alias', name)
-            name = alias if isinstance(alias, STRING_TYPES) else alias[0]
+            if isinstance(alias, STRING_TYPES):
+                name = alias 
+            elif isinstance(alias[0], STRING_TYPES):
+                name = alias[0]
+            else:
+                name = alias[0][0]
 
             # add name
             ts = type_to_str(info['type'])
